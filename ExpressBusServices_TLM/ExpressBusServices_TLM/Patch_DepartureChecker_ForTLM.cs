e77bea32c9a93b64459a90ea0018fe2d7943a119
Vectorial1024 <vincentwwy168@gmail.com>
2021-10-17 23:06:50 +0800

    Fill in TLM compatibility

---
+++ ExpressBusServices_TLM/ExpressBusServices_TLM/Patch_DepartureChecker_ForTLM.cs
@@ -0,0 +1,20 @@
+﻿using ExpressBusServices;
+using HarmonyLib;
+using Klyte.TransportLinesManager.Extensions;
+using UnityEngine;
+
+namespace ExpressBusServices_TLM
+{
+    [HarmonyPatch(typeof(DepartureChecker))]
+    [HarmonyPatch("StopIsTerminus", MethodType.Normal)]
+    public class Patch_DepartureChecker_ForTLM
+    {
+        // post fix the "is this a terminus stop" to cater for TLM terminus cases
+        [HarmonyPostfix]
+        public static void PostFix(ref bool __result, ushort stop)
+        {
+            // could this be a TLM bus terminus?
+            __result = TLMStopDataContainer.Instance.SafeGet(stop).IsTerminal;
+        }
+    }
+}
