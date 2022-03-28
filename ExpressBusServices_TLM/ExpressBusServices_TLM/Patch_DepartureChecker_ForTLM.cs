7b83bfff191c00c37257aedd420b2fa2c36f1532
Vectorial1024 <vincentwwy168@gmail.com>
2022-03-28 16:28:30 +0800

    Fix logic error

--- ExpressBusServices_TLM/ExpressBusServices_TLM/Patch_DepartureChecker_ForTLM.cs
+++ ExpressBusServices_TLM/ExpressBusServices_TLM/Patch_DepartureChecker_ForTLM.cs
@@ -14,7 +14,10 @@ namespace ExpressBusServices_TLM
         public static void PostFix(ref bool __result, ushort stopID)
         {
             // could this be a TLM bus terminus?
-            __result &= TLMStopDataContainer.Instance.SafeGet(stopID).IsTerminal;
+            // note that the first stop in line is not a "terminus by computation"; instead, it is a "terminus by definition"
+            // and so if we use this "terminus by computation" method, we will not read first stops correctly.
+            // we will also need to preserve the value from our base mod
+            __result |= TLMStopDataContainer.Instance.SafeGet(stopID).IsTerminal;
         }
     }
 }
