f0ae6220726c382027d1d2959dca558247dff5bc
Vincent Wong <vincentwwy168@gmail.com>
2025-05-30 22:30:16 +0800

    Clean up the code; bump version

--- ExpressBusServices_TLM/ExpressBusServices_TLM/DepartureChecker_TLM.cs
+++ ExpressBusServices_TLM/ExpressBusServices_TLM/DepartureChecker_TLM.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using JetBrains.Annotations;
 using TransportLinesManager.Data.DataContainers;
 
 namespace ExpressBusServices_TLM
 {
+    [UsedImplicitly]
     public class DepartureChecker_TLM
     {
+        [UsedImplicitly]
         public static bool TLM_StopIsTerminus(ushort stopID)
         {
             // could this be a TLM bus terminus?
-            // architecture reversal: th main mod will read this value to decide things.
+            // architecture reversal: the main mod will read this value to decide things.
             return TLMStopDataContainer.Instance.SafeGet(stopID).IsTerminal;
         }
     }
